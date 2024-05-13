583f8bc082389e580b36d3ffd5fe043f1d07a400
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-05-13 12:33:44 +0900

    Add : 캐릭터 애니메이션 추가
    Add Character Animation

---
+++ Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterAnimationController : AnimationController
+{
+    private static readonly int isWalking = Animator.StringToHash("isWalking");
+
+    private readonly float magnituteThreshold = 0.5f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 vector)
+    {
+        animator.SetBool(isWalking, vector.magnitude > magnituteThreshold);
+    }
+}
